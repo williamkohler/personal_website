66efdd427c8626dca62d7260ca923cd8c88ac32c
Bill Kohler <bkohler4@gmail.com>
2018-02-06 23:01:56 -0500

    update test page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,4 @@
 class StaticPagesController < ApplicationController
+  def test
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get '/test', to: 'static_pages#test'
 end
